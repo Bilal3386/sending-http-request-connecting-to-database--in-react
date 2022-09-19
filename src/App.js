f7afec2add6bdd650065cab07b781705b53d889a
mohdbilal <mohdbilal3386@gmail.com>
2022-09-20 04:04:04 +0530

    Storing and deleting the new movies into the Db

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-
 import MoviesList from "./components/MoviesList";
 import "./App.css";
 import { useCallback } from "react";
@@ -11,14 +10,14 @@ function App() {
   const [error, setError] = useState(null);
   // const [stop, setStop] = useState(null);
 
- 
+
 
   const  fetchMoviesHandler = useCallback(async() =>   {
     setIsLoader(true);
     setError(null);
     // setStop(null);
     try {
-      const response = await fetch("https://swapi.py4e.com/api/films/");
+      const response = await fetch("https://react-http-d4523-default-rtdb.firebaseio.com/movies.json");
       if (!response.ok) {
 
         throw new Error("Something went wrong ....Retrying");
@@ -26,16 +25,26 @@ function App() {
       
 
       const data = await response.json();
-
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+ 
+      const loadedMovies = []
+      for(const key in data) 
+      {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        })
+      }
+      // const transformedMovies = data.results.map((movieData) => {
+      //   return {
+      //     id: movieData.episode_id,
+      //     title: movieData.title,
+      //     openingText: movieData.opening_crawl,
+      //     releaseDate: movieData.release_date,
+      //   };
+      // });
+      setMovies(loadedMovies);
       setIsLoader(false);
     } catch (error) {
       setError(error.message);
@@ -49,6 +58,10 @@ function App() {
 
   useEffect(() => {
     fetchMoviesHandler()
+    // const interval = setInterval(() =>
+    //  {fetchMoviesHandler()}, 5000)
+    //  console.log('Rerendering')
+    //  return () => clearInterval(interval)
   }, [fetchMoviesHandler]);
 
   // const stopRetryingHandler = () => {
@@ -56,9 +69,33 @@ function App() {
   //   clearInterval(stop);
   // };
 
+
+  const addMovieHandler = async (movie) => {
+    const response = await fetch('https://react-http-d4523-default-rtdb.firebaseio.com/movies.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers:  {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const data = await response.json()
+
+    console.log(data)
+  }
+  
+
+  const deleteMovieHandler = async(id) => {
+    const response = await fetch(`https://react-http-d4523-default-rtdb.firebaseio.com/movies/${id}.json`, {
+      method: 'DELETE',
+    })
+    //console.log(response)
+    const data = await response.json()
+    console.log(data)
+  }
   let content = <p>Found no movies</p>;
   if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
+    content = <MoviesList onChange={deleteMovieHandler} movies={movies} />;
   }
   if (error) {
     content = <p>{error}</p>;
@@ -69,7 +106,7 @@ function App() {
   return (
     <React.Fragment>
     <section>
-    <AddMovieForm />
+    <AddMovieForm onAddMovies={addMovieHandler}/>
     </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
--- src/components/AddMovieForm.js
+++ src/components/AddMovieForm.js
@@ -1,38 +1,41 @@
-import React, {useState} from 'react'
+import React, {useRef} from 'react'
 import classes from './AddMovieForm.module.css'
 
-const AddMovieForm = () => {
+const AddMovieForm = props => {
 
-    const [title, setTitle] = useState('')
-    const [openingText, setOpeningText] = useState('')
-    const [releaseDate, setReleaseDate] = useState('')
+    const titleRef = useRef('')
+    const openingTextRef = useRef('')
+    const  releaseDateRef = useRef('')
+    // const [title, setTitle] = useState('')
+    // const [openingText, setOpeningText] = useState('')
+    // const [releaseDate, setReleaseDate] = useState('')
 
-    const titleHandler = (event) => {
-        setTitle(event.target.value)
-    }
+    // const titleHandler = (event) => {
+    //     setTitle(event.target.value)
+    // }
 
-    const openingTextHandler = (event) => {
-        setOpeningText(event.target.value)
-    }
+    // const openingTextHandler = (event) => {
+    //     setOpeningText(event.target.value)
+    // }
 
-    const releaseDateHandler = (event) => {
-        setReleaseDate(event.target.value)
-    }
+    // const releaseDateHandler = (event) => {
+    //     setReleaseDate(event.target.value)
+    // }
 
 
     const submitHandler = event => {
         event.preventDefault()
-        const newMovieObj = {title: title, openingText: openingText, releaseDate: releaseDate}
-        console.log(newMovieObj)
+        const newMovieObj = {title: titleRef.current.value, openingText: openingTextRef.current.value, releaseDate: releaseDateRef.current.value}
+        props.onAddMovies(newMovieObj)
     }
   return (
     <form className={classes.control} onSubmit={submitHandler}>
-    <label>Title</label>
-    <input type='text' name='title' onChange={titleHandler}/>
-    <label>opening Text</label>
-    <textarea type='text' name='openingText' onChange={openingTextHandler}/>
+    <label htmlFor='title'>Title</label>
+    <input type='text' id='title' ref={titleRef}/>
+    <label htmlFor='opening-text'>opening Text</label>
+    <textarea row='5' type='text' name='openingText' ref={openingTextRef}/>
     <label>Release Date</label>
-    <input type='text' name='releasingDate' onChange={releaseDateHandler}/>
+    <input type='text' name='releasingDate' ref={releaseDateRef}/>
     <button type='submit'>Add Movie</button>
     </form>
   )
